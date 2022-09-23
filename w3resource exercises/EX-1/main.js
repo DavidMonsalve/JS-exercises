edf8977c0850b8c0f0ad58f967a3066b6ab0961c
David Monsalve <davidmonsalve717@gmail.com>
2022-09-23 20:22:47 -0300

    first commit

--- w3resource exercises/EX-1/main.js
+++ w3resource exercises/EX-1/main.js
@@ -1,5 +1,20 @@
 const weekday = ["sunday", "monday", "wednesday", "thursday", "friday", "saturday"]
+const day = new Date()
+const currentDay = day.getDay()
+const hour = day.getHours()
+const minutes = day.getMinutes()
+const sec = day.getSeconds()
 
- const day = new Date()
- console.log("Today is:", weekday[day.getDay()])
- console.log("Current time is:", day.getHours(), "PM :", day.getMinutes(), ":", day.getSeconds())
+var meridian = ""
+
+if (hour >= 12) {
+    meridian = "PM"
+} else{
+    meridian = "AM"
+}
+
+ console.log("Today is:", weekday[currentDay])
+
+
+
+ console.log("Current time is:", hour, meridian, minutes, ":", sec)
