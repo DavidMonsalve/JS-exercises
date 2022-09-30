cf59feb83798777bb524e007e5c7b0c3d7b0c552
David Monsalve <davidmonsalve717@gmail.com>
2022-09-29 23:26:20 -0300

    ex-9

---
+++ w3resource exercises/EX-9/main.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+const btnMultiply = document.querySelector("multiply")
+const btnDivide = document.querySelector("divide")
+
+btnDivide.addEventListener("click", myFunction);
+
+function myFunction() {
+    alert("puta")
+}
